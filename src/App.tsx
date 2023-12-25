bd6b1b009cc436cbcf95396ece2937f5e9de52a0
sohei-fujita <nnn.sohei.fujita@gmail.com>
2023-12-25 18:10:09 +0900

    display form

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <Form />
     </div>
   );
 }
