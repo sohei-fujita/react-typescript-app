ecd140f7e0058ac5c30e9c9c5e49aa3f10aa52c9
sohei-fujita <nnn.sohei.fujita@gmail.com>
2023-12-29 21:45:40 +0900

    get api

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 
 const Form = () => {
   const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch("https://api.weatherapi.com/v1/current.json?key=f39bb18e7aab44aeb78115328232912&q=London&aqi=no")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
   return (
     <form>
       <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} />
-      <button type="submit">Get Weather</button>
+      <button type="submit" onClick={getWeather}>Get Weather</button>
     </form>
   );
 };
