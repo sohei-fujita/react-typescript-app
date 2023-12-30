ca84404a03ecbce10bb72589e191d7739cdbee2d
SFujita <fufufujita@fujita-MB.local>
2023-12-30 16:45:19 +0900

    make result component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@
 import './App.css';
 import Title from './components/Title';
 import Form from './components/Form';
+import Results from './components/Result';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <Form />
+      <Results />
     </div>
   );
 }
