4ca7e94c25892c7914df49c0d97420cc2c01e622
SFujita <fufufujita@fujita-MB.local>
2023-12-31 13:13:15 +0900

    set type

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,21 @@ import './App.css';
 import Title from './components/Title';
 import Form from './components/Form';
 import Results from './components/Result';
+import { useState } from "react";
 
 function App() {
+  const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch("https://api.weatherapi.com/v1/current.json?key=f39bb18e7aab44aeb78115328232912&q=London&aqi=no")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
   return (
     <div className="App">
       <Title />
-      <Form />
+      <Form setCity={setCity} getWeather={getWeather}/>
       <Results />
     </div>
   );
