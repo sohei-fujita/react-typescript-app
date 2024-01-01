34a9a1cc0090b96f0af6432f21ffc05035080ce4
[sohei-fujita] <nnn.sohei.fujita@gmail.com>
2024-01-01 17:05:45 +0900

    set const

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,13 @@ import { useState } from "react";
 
 function App() {
   const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
   const getWeather = (e: any) => {
     e.preventDefault();
     fetch("https://api.weatherapi.com/v1/current.json?key=f39bb18e7aab44aeb78115328232912&q=London&aqi=no")
