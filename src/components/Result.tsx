ce38d11b452eaaef2f1ecdac16c201e3f398bdba
[sohei-fujita] <nnn.sohei.fujita@gmail.com>
2024-01-05 20:24:14 +0900

    display information

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -10,7 +10,12 @@ type ResultsPropsType = {
 
 const Results = (props: ResultsPropsType) => {
   return (
-    <h1>気象データ{props.results.country}</h1>
+    <div>
+      {props.results.country && <div>{props.results.country}</div>}
+      {props.results.cityName && <div>{props.results.cityName}</div>}
+      {props.results.temperature && <div>{props.results.temperature}<span>°C</span></div>}
+      {props.results.conditionText && <div><img src={props.results.icon} alt="icon" /><span>{props.results.conditionText}</span></div>}
+    </div>
   );
 };
 
