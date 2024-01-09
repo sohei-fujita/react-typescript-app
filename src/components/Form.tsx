195ad02cd4b282f5c2846b4bd8ae7384b6ff5482
[sohei-fujita] <nnn.sohei.fujita@gmail.com>
2024-01-09 20:07:30 +0900

    値が空の時、ボタンがdisabled

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,24 @@
+import React, { useState } from 'react';
+
 type FormPropsType = {
   setCity: React.Dispatch<React.SetStateAction<string>>;
   getWeather: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
 const Form = (props: FormPropsType) => {
+  const [inputValue, setInputValue] = useState<string>(''); // 新しいステートを追加
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    props.setCity(e.target.value);
+  };
+
   return (
     <form onSubmit={props.getWeather}>
-      <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)} />
-      <button type="submit">Get Weather</button>
+      <input type="text" name="city" placeholder="都市名" value={inputValue}
+      // onChange={e => props.setCity(e.target.value)} />
+      onChange={handleInputChange} />
+      <button type="submit" disabled={!inputValue.trim()} >Get Weather</button>
     </form>
   );
 };
