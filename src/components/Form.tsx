ec2c06d83be0fc22930a17b8c96dce0fa0ed7f01
[sohei-fujita] <nnn.sohei.fujita@gmail.com>
2024-01-21 20:39:45 +0900

    replace input box

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -13,10 +13,21 @@ const Form = (props: FormPropsType) => {
     props.setCity(e.target.value);
   };
 
+  const [selectedCity, setSelectedCity] = useState('');
+
+  const handleCityChange = (e: any) => {
+    setSelectedCity(e.target.value);
+  };
+
   return (
     <form onSubmit={props.getWeather}>
-      <input type="text" name="city" placeholder="都市名" value={inputValue}
-      onChange={handleInputChange} />
+      {/* <input type="text" name="city" placeholder="都市名" value={inputValue}
+      onChange={handleInputChange} /> */}
+      <select name="city" value={selectedCity} onChange={handleCityChange}>
+        <option value="tokyo">Paris</option>
+        <option value="osaka">Tokyo</option>
+        <option value="kyoto">London</option>
+      </select>
       <button type="submit" disabled={!inputValue.trim()} >Get Weather</button>
     </form>
   );
